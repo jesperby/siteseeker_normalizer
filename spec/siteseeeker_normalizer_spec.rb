51d308d2bc762be491254e21843abbfbd2bcdd3a
martent <marten@thavenius.se>
2013-11-26 14:37:13 +0100

    Test agains local dumped results

--- spec/siteseeeker_normalizer_spec.rb
+++ spec/siteseeeker_normalizer_spec.rb
@@ -3,99 +3,67 @@ require 'spec_helper'
 
 describe SiteseekerNormalizer do
   before(:each) do
-    @client = SiteseekerNormalizer::Client.new(SPEC_CONFIG["account"], SPEC_CONFIG["index"])
-    @response = @client.search("barn")
+    raw_results = open("spec/fixtures/barn.html").read
+    @results = SiteseekerNormalizer::Parse.new(raw_results, encoding: "UTF-8")
   end
 
   it "should have a number of hits" do
-    @response.total.should be_a Fixnum
+    @results.total.should be_a Fixnum
   end
 
   it "should have results" do
-    @response.entries.count.should > 0
+    @results.entries.count.should > 0
   end
 
   describe "result entry" do
     it "should have an order number" do
-      @response.entries.first.number.should eq 1
+      @results.entries.first.number.should eq 1
     end
 
     it "should have a title" do
-      @response.entries.first.title.should be_a String
+      @results.entries.first.title.should be_a String
     end
 
     it "should have an extract" do
-      @response.entries.first.summary.should be_a String
+      @results.entries.first.summary.should be_a String
     end
 
     it "should have a breadcrumb" do
-      @response.entries.first.breadcrumbs.should be_an Array
+      @results.entries.first.breadcrumbs.should be_an Array
     end
 
     it "should have a category" do
-      @response.entries.first.category.should be_a String
+      @results.entries.first.category.should be_a String
     end
 
     it "should have a date string" do
-      @response.entries.first.date.should be_a String
+      @results.entries.first.date.should be_a String
     end
   end
 
   it "should have sorting" do
-    @response.sorting.should be_an Array
+    @results.sorting.should be_an Array
   end
 
   it "should have a first sorting entry with text" do
-    @response.sorting.first.text.should be_a String
+    @results.sorting.first.text.should be_a String
   end
 
   it "should have a second sorting entry with an url" do
-    @response.sorting[1].query.should be_a String
+    @results.sorting[1].query.should be_a String
   end
 
   it "should have a query string for getting more results" do
-    @response.more_query.should be_a String
+    @results.more_query.should be_a String
   end
 
   it "should have a categories" do
-    @response.category_groups.should be_an Array
-  end
-
-  it "should also take an array as a search argument" do
-    response = @client.search({q: "barn"})
-    response.total.should > 0
+    @results.category_groups.should be_an Array
   end
 
   it "should show a spelling suggestions" do
-    response = @client.search("barnomsrg")
-    response.suggestions.count.should > 0
-  end
-
-  it "should raise an error for an invalid search url" do
-    expect {
-      client = SiteseekerNormalizer::Client.new("x")
-      response = client.search("y")
-    }.to raise_error
-  end
-
-  it "should raise an error for an invalid account name" do
-    expect {
-      client = SiteseekerNormalizer::Client.new("x", SPEC_CONFIG["index"])
-      response = client.search("y")
-    }.to raise_error(SocketError)
-  end
-
-  it "should raise an error for an invalid index name" do
-    expect {
-      client = SiteseekerNormalizer::Client.new(SPEC_CONFIG["account"], "foo")
-      response = client.search("y")
-    }.to raise_error(OpenURI::HTTPError)
-  end
-
-  it "should raise a timeout error" do
-    expect {
-      client = SiteseekerNormalizer::Client.new(SPEC_CONFIG["account"], SPEC_CONFIG["index"], read_timeout: 0.01)
-      response = client.search("y")
-    }.to raise_error(Timeout::Error)
+    raw_results = open("spec/fixtures/barnomsrg.html").read
+    results = SiteseekerNormalizer::Parse.new(raw_results, encoding: "UTF-8")
+    results.suggestions.count.should > 0
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
 require "siteseeker_normalizer"
 require 'nokogiri'
 require "yaml"
-
-begin
-  SPEC_CONFIG = YAML.load_file("./spec/spec_config.yml")
-rescue
-  puts "ERROR: You need to copy the spec/spec_config.yml.example file"
-  puts "       and add your Siteseeker account before running the tests."
-  exit
-end
